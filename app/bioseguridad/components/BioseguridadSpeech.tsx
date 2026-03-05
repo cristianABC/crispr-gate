c05105508076ce1d6f3acbb34c8a635a1bb2c5ff
2mxas <t.hernandez11@uniandes.edu.co>
2026-03-05 06:59:07 -0500

    Componente speech agregado

---
+++ app/bioseguridad/components/BioseguridadSpeech.tsx
@@ -0,0 +1,41 @@
+import { useRef, useState } from 'react'
+
+export default function BioseguridadSpeech() {
+    const [texto, setTexto] = useState("")
+    const [escuchando, setEscuchando] = useState(false)
+    const recognitionRef = useRef<any>(null)
+
+    const iniciar = () => {
+        // Inicialización (Verificar compatibilidad con prefijos)
+        const Speech = (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition;
+        
+        const recognition = new Speech();
+        recognition.lang = "es-CO";
+        recognition.continuous = true
+        recognition.interimresults = false
+
+        // Uso: Captura de datos
+        recognition.onresult = (event: any) => {
+        const resultado = event.results[0][0].transcript;
+        setTexto(resultado);
+        };
+
+        recognition.start()
+        recognitionRef.current = recognition
+        setEscuchando(true)
+    }
+
+    const detener = () => {
+        recognitionRef.current?.stop()
+        setEscuchando(false)
+    }
+  return (
+    <div>
+        <div>
+            <button onClick={escuchando? detener : iniciar}>
+                {escuchando? "INICIAR DICTADO" : "ESCANEANDO VOZ..."}
+            </button>
+        </div>
+    </div>
+  )
+}
