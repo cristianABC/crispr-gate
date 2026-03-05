af9706b036e95988c3bb8c43b617e7a7d6d1b1d3
DiegoMunevar2007 <d.munevar@uniandes.edu.co>
2026-03-05 07:45:03 -0500

    Cambios Form

--- app/genoma-id/page.tsx
+++ app/genoma-id/page.tsx
@@ -1,8 +1,10 @@
+import CrearFeedback from "./componentes/CrearFeedback";
 
 export default function GenomaIdPage() {
   return (
     <div>
       <h1>Hello Page</h1>
+      <CrearFeedback></CrearFeedback>
     </div>
   );
 }
