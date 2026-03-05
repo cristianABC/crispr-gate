b1835c59522f74a6f1e7788ae37d3d0ff7863672
Manu_Love <107737902+mloveral@users.noreply.github.com>
2026-03-05 07:39:27 -0500

    tenemos visualización

--- app/genoma-id/page.tsx
+++ app/genoma-id/page.tsx
@@ -1,13 +1,8 @@
-import { ViajeroIdentidad } from "@/types/types.interface";
-import { GET } from "../api/genoma-id/route";
-import LabRecordCard from "./components/lab-record-card";
 
 export default function GenomaIdPage() {
-    const mock: ViajeroIdentidad = { id: "ID-201", name: "Kaelen Voss", dnaPurity: 98, modifications: ["Filtro respiratorio", "Filtro respiratorio"], riskLevel: "CRITICAL", lastScan: "2026-03-01" }
-
     return (
         <div>
-            <LabRecordCard labRecord={mock} />
+            
         </div>
     );
 }
