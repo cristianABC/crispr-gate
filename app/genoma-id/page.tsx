122d54a1ce677f8c73e6aea95df1749816019151
DiegoMunevar2007 <d.munevar@uniandes.edu.co>
2026-03-05 07:51:45 -0500

    A

--- app/genoma-id/page.tsx
+++ app/genoma-id/page.tsx
@@ -1,10 +1,12 @@
 import CrearFeedback from "./componentes/CrearFeedback";
+import MediaDevices from "./media_devices";
 
 export default function GenomaIdPage() {
   return (
     <div>
       <h1>Hello Page</h1>
       <CrearFeedback></CrearFeedback>
+      <MediaDevices></MediaDevices>
     </div>
   );
 }
